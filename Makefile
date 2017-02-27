46ea0bf1bd34f4e81c4f62fabd6f2b4dc759ab82
Phil Mansfield <mansfield@uchicago.edu>
2017-02-27 16:37:42 -0600

    Initial commit with dummy fiels.

---
+++ Makefile
@@ -0,0 +1,83 @@
+# Author: Phil Mansfield
+#
+# How to use:
+# make - puts a library file in ./build
+# make test - runs all tests and benchmarks in ./test. Assumes the existence
+#              of a particular script called runTests.py in that scripts/.
+# make clean - removes temporary files and allows for a clean build.
+#
+# How to use for a new project:
+# Go to the other commented areas in this file and do what they say.
+
+CC=gcc
+CFLAGS=-O2 -Wall -Wextra -Werror -std=c99 -g
+
+SOURCES=$(wildcard src/*.c)
+OBJECTS=$(patsubst %.c,%.o,$(SOURCES))
+HEADERS=$(patsubst %.c,%.h,$(SOURCES))
+
+# Change this as neccesary. If you aren't making a library, the vast majority of
+# this Makefile will be useless to you. Name will be build/*lib.a
+LIB_NAME=minnow
+
+TARGET=$(patsubst %,build/lib%.a,$(LIB_NAME))
+SELF_FLAG=$(patsubst %,-l%,$(LIB_NAME))
+
+SO_TARGET=$(patsubst, %.a,%.so,$(TARGET))
+
+TEST_SOURCES=$(wildcard test/*.c)
+TESTS=$(patsubst %.c,%,$(TEST_SOURCES))
+
+# Location of libraries being used.
+LIBRARIES=
+# Flags of libraries being used.
+LIBRARY_FLAGS=
+# Location of .h files which should be included.
+INCLUDES=
+
+ifneq ($(INCLUDES),)
+	INCLUDES_WITH_FLAG=-I $(INCLUDES)
+else
+	INCLUDES_WITH_FLAG=$(INCLUDES)
+endif
+
+all: $(TARGET) $(SO_TARGET)
+
+debug: CFLAGS += -g -D DEBUG
+debug: all
+
+debug_test: CFLAGS += -g -D DEBUG
+debug_test: test
+
+build:
+	mkdir -p build/
+
+# Change this as needed for each object file. List dependencies that are
+# not the corresponding .c and .h file.
+tmp.o:
+%.o: %.c %.h Makefile
+	$(CC) $(CFLAGS) -c -o $@ $< $(INCLUDES_WITH_FLAG)
+
+$(TARGET): build $(OBJECTS) $(HEADERS)
+	ar rcs $@ $(OBJECTS)
+	ranlib $@
+
+$(SO_TARGET): $(CFLAGS) += -fPIC
+$(SO_TARGET): build $(OBJECTS) $(HEADERS)
+	$(CC) -shared -o $@ $(OBJECTS)
+
+%_test: %_test.c $(TARGET)
+	$(CC) $@.c -o $@ $(CFLAGS) -L build $(LIBRARIES) -I src $(INCLUDES) $(SELF_FLAG) $(LIBRARY_FLAGS)
+%_bench: %_bench.c $(TARGET)
+	$(CC) $@.c -o $@ $(CFLAGS) -L build $(LIBRARIES) -I src $(INCLUDES) $(SELF_FLAG) $(LIBRARY_FLAGS)
+
+test: $(TESTS) scripts/run_tests.py
+	@python scripts/run_tests.py test/ test
+	@python scripts/run_tests.py test/ bench
+
+clean:
+	rm -rf build/
+	rm -f  src/*.o
+	rm -f  test/*_test
+	rm -f  test/*_bench
+	rm -rf test/*.dSYM
---
+++ src/debug.h
@@ -0,0 +1,88 @@
+#ifndef MNW_DEBUG_
+#define MNW_DEBUG_
+
+/* Author: Phil Mansfield (mansfield@uchicago.edu)
+ *
+ * `debug.h` contains many useful debugging macros.
+ */
+
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#if defined(__GNUC__) || defined(__linux__)
+#include <execinfo.h>
+#endif /* __GNUC__ || __linux__ */
+
+/* `Assert` is an assertion which allows you to print messages an clean up
+ * prior to failure. An example usage would be the following:
+ * 
+ * Assert(myPointer) {
+ *     fprintf(stderr, "myPointer is NULL.");
+ * }
+ *
+ * If you want a stack trace (which you should), prefer to use Panic instead
+ * of fprintf for reporting your error.
+ */
+#define Assert(x) for (; (!(x)); assert(x))
+
+/* `AssertAlloc` is an assertion which specifically checks whether an allocation
+ * error has occured. Unlike Panic, it does not assume that allocators still
+ * work. All alloctation in the code should look pretty much exactly like this:
+ * 
+ * int64_t *ptr = calloc(num, size);
+ * AssertAlloc(ptr);
+ */
+#define AssertAlloc(ptr) \
+    do { \
+        ASSERT(ptr) { \
+            fprintf(stderr, "%s: %s: L%d: Allocation failed\n.", \
+                    __FILE__, __FUNCTION__, __LINE__);           \
+            exit(1); \
+        } \
+    } while(0)
+
+/* `DebugAssert` and `DebugPrintf` are `Assert` and `printf` statements
+ * (respectively), which only run if the `DEBUG` flag is set at compile time.
+ * They are otherwise identical.
+ */
+#ifdef DEBUG
+#define DebugAssert(x) assert(Assert)
+#define DebugPrintf(fmt, ...) printf(fmt, ##__VA_ARGS__)
+#else /* !DEBUG */
+#define DebugAssert(expr)
+#define DebugPrintf(fmt, ...)
+#endif /* DEBUG */
+
+/* Panic crashes the program and prints out descriptive information. On some
+ * compilers/systems (read: GCC), this might include a stack trace. Panic
+ * may require dynamic memory allocation, but also kills the program, so this
+ * is only a problem if your allocator is currently compromised.
+ */
+#if defined(__GNUC__) || defined(__linux__)
+#define Panic(fmt, ...) \
+    do { \
+        /* This is a bit excessive, but I'd prefer to avoid linking. */ \
+        fprintf(stderr, "Panic at file %s, function %s, line %d:\n", \
+                __FILE__, __FUNCTION__, __LINE__);               \
+        fprintf(stderr, fmt, ##__VA_ARGS__); \
+        fprintf(stderr, "Stack trace:\n"); \
+        void **panicStackframes_ = calloc(100, sizeof(*panicStackframes_)); \
+        size_t panicSize_ = backtrace(panicStackframes_, 100); \
+        char **panicStrings_ = backtrace_symbols( \
+            panicStackframes_, panicSize_ \
+        );                                                              \
+        for (size_t panicIdx_ = 0; panicIdx_ < panicSize_; panicIdx_++) \
+            fprintf(stderr, "%s\n", panicStrings_[panicIdx_]);           \
+        free(panicStrings_); \
+        exit(1); \
+    } while (0)
+#else /* __GNUC__ || __linux__ */
+#define Panicf(fmt, ...) \
+    do { \
+        fprintf(stderr, "%s:%s:L%d:\n", __FILE__, __FUNCTION__, __LINE__); \
+        fprintf(stderr, fmt, ##__VA_ARGS__); \
+        exit(1); \
+    } while (0)
+#endif /* __GNUC__ || __linux__ */
+
+#endif /* MNW_DEBUG_*/
