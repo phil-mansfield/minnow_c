a1f64a0db2b153a5ff25b7656ace042dc3af0c3b
phil-mansfield <mansfield@uchicago.edu>
2017-03-21 16:51:20 -0500

    Fixed invalid memory access bug in Quantize()

--- src/algo.c
+++ src/algo.c
@@ -581,10 +581,12 @@ algo_QuantizedParticles Quantize(
         FSeq var = p.FVars.Data[i];
         if (var.Len == 0) { continue; }
 
+		printf("%d %g %g\n", i, buf.FVarsRange[i].X0, buf.FVarsRange[i].X1);
+		
         float f0 = buf.FVarsRange[i].X0;
         float df = buf.FVarsRange[i].X1 - buf.FVarsRange[i].X0;
         if (buf.FVarsRange[i].Depths.Len == 0) {
-            buf.FVars.Data[i] = util_UniformBinIndex(
+		    buf.FVars.Data[i] = util_UniformBinIndex(
                 var, buf.FVarsRange[i].Depth, f0, df, buf.FVars.Data[i]
             );
         } else {
