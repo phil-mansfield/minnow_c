da4524dc52ee39d47f3687b310d6097ce0851d67
Phil Mansfield <mansfield@uchicago.edu>
2017-04-28 11:06:43 -0500

    Wrote skeletons for algo_Test

---
+++ scratch/algo_Test_v0_9.c
@@ -0,0 +1,17 @@
+#include "algo_Test_v0_9.h"
+
+CField TestCompress_v0_9(QField cf, void *buffer) {
+    (void) cf;
+    (void) buffer;
+
+    CField dummy;
+    return dummy;
+}
+
+QField TestDecompress_v0_9(CField cf, void *buffer) {
+    (void) cf;
+    (void) buffer;
+    
+    QField dummy;
+    return dummy;
+}
---
+++ scratch/algo_Test_v0_9.h
@@ -0,0 +1,9 @@
+#ifndef MNW_TEST_V0_9_H_
+#define MNW_TEST_V0_9_H_
+
+#include "types.h"
+
+CField TestCompress_v0_9(QField cf, void *buffer);
+QField TestDecompress_v0_9(CField cf, void *buffer);
+
+#endif
